e032a879e2b6b5e1ac2993aaa066d86ebdf0b67a
Mamunuz Zaman <mzs.munna@gmail.com>
2025-11-26 10:38:08 +0600

    result pattern implementation - ongoing

--- apps/tasker/src/Tasker.Domain/Models/Result.cs
+++ apps/tasker/src/Tasker.Domain/Models/Result.cs
@@ -6,34 +6,34 @@ namespace Tasker.Domain.Models
 {
     public class Result<T>
     {
-        private readonly T? _value;
+        private readonly T? _data;
         private readonly Error? _error;
         private readonly bool _isSuccess;
 
-        private Result(T value)
+        private Result(T data)
         {
             _isSuccess = true; 
-            _value = value;
+            _data = data;
             _error = Error.None;
         }
 
         private Result(Error error)
         {
             _isSuccess = false; 
-            _value = default;
+            _data = default;
             _error = error;
         }
 
-        public static implicit operator Result<T>(T value) => new(value);
+        public static implicit operator Result<T>(T data) => new(data);
         public static implicit operator Result<T>(Error error) => new(error);
 
-        public static Result<T> Success(T value) => new Result<T>(value);
+        public static Result<T> Success(T data) => new Result<T>(data);
         public static Result<T> Failure(Error error) => new Result<T>(error);
         public TResult Map<TResult>(
             Func<T, TResult> success,
             Func<Error, TResult> failure)
         {
-            return _isSuccess ? success(_value!) : failure(_error!);
+            return _isSuccess ? success(_data!) : failure(_error!);
         }
     }
 }
