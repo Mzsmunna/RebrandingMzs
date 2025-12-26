1bea3203a93211cc464c1f696461acdd9c6b442a
Mamunuz Zaman <mzs.munna@gmail.com>
2025-12-26 22:27:42 +0600

    ef migrrations file locationn added

---
+++ apps/tasker/src/Tasker.Infrastructure/DB/SqlServer/Migrations/SqlServerEFMigrations.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasker.Infrastructure.DB.SqlServer.Migrations
+{
+    internal class SqlServerEFMigrations
+    {
+    }
+}
