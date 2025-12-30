cdb7d940818c4b3962763fdd14eb74aa52222fdd
Mamunuz Zaman <mzs.munna@gmail.com>
2025-12-30 21:54:10 +0600

    code refactoring

--- mzstruct/Mzstruct.Common/Dependencies/AuthResolver.cs
+++ mzstruct/Mzstruct.Common/Dependencies/AuthResolver.cs
@@ -77,12 +77,9 @@ namespace Mzstruct.Common.Dependencies
                         OnMessageReceived = context =>
                         {
                             var accessToken = context.Request.Query["access_token"];
-
                             var path = context.HttpContext.Request.Path;
-                            if (!string.IsNullOrEmpty(accessToken) && (path.StartsWithSegments("/jwtevents")))
-                            {
-                                context.Token = accessToken;
-                            }
+                            if (!string.IsNullOrEmpty(accessToken) 
+                            && (path.StartsWithSegments("/jwtevents"))) context.Token = accessToken;
                             return Task.CompletedTask;
                         }
                     };
