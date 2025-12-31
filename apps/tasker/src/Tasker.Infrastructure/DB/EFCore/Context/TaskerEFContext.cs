4bd592361ffe35be43e01d013f50cf795b1aadf1
Mamunuz Zaman <mzs.munna@gmail.com>
2025-12-31 11:27:57 +0600

    code refactoring

---
+++ apps/tasker/src/Tasker.Infrastructure/DB/EFCore/Context/TaskerEFContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mzstruct.Base.Enums;
+using Mzstruct.DB.ORM.EFCore.Context;
+using Tasker.Application.Features.Issues;
+using Tasker.Application.Features.Users;
+
+namespace Tasker.Infrastructure.DB.EFCore.Context
+{
+    public class TaskerEFContext : EFContext
+    {
+        public TaskerEFContext(DbContextOptions options) : base(options, DBType.SqlServer) { }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Issue> Issues { get; set; }
+    }
+}
