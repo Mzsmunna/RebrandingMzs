14fb8e80a99646e206ae1915a26509ebd51a91d8
Mamunuz Zaman <mzs.munna@gmail.com>
2026-01-16 16:19:51 +0600

    Identity framework - ongoing

--- mzstruct/Mzstruct.DB/EFCore/Context/AppDBContext.cs
+++ mzstruct/Mzstruct.DB/EFCore/Context/AppDBContext.cs
@@ -3,24 +3,6 @@ using Mzstruct.Base.Models;
 using Mzstruct.DB.Contracts.IContext;
 using Mzstruct.DB.EFCore.Helpers;
 
-/// <commands> .NET & EF CLI  </commands>
-/// dotnet tool list --global | -g
-/// dotnet tool install --global dotnet-ef --version 7.0.11
-/// dotnet add package Microsoft.EntityFrameworkCore --version 7.0.11
-/// dotnet add package Microsoft.EntityFrameworkCore.Design --version 7.0.11
-/// dotnet ef --version
-/// 
-/// dotnet ef migrations add InitialCreate --output-dir Infrastructure/DB/Migrations
-/// dotnet ef migrations add InitialCreate --project apps/tasker/src/Tasker.Infrastructure --startup-project apps/tasker/src/Tasker.Presentation/Tasker.RestAPI --output-dir DB/SqlServer/Migrations/
-/// dotnet ef database update -p apps/tasker/src/Tasker.Infrastructure -s apps/tasker/src/Tasker.Presentation/Tasker.RestAPI
-/// dotnet ef database update
-/// dotnet ef database update -p Mzstruct.DB.SQL -s Mzstruct.Api --context DatabaseContext
-/// 
-/// <commands> Package Manager Console </commands>
-/// Add-Migration InitialCreate
-/// Update-Databse | Update-Database -Migration Name -ConnectionString
-/// Bundle-Migration -ConnectionString
-/// 
 namespace Mzstruct.DB.EFCore.Context
 {
     public class AppDBContext<TContext> : DbContext, IAppDBContext where TContext : DbContext
@@ -30,17 +12,17 @@ namespace Mzstruct.DB.EFCore.Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            base.OnConfiguring(optionsBuilder);
             //optionsBuilder.LogTo(Console.WriteLine);
             //EFCoreHelper.OnDefaultConfiguring(optionsBuilder, dbType);
-            base.OnConfiguring(optionsBuilder);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            EFCoreHelper.OnDefaultModelCreating(modelBuilder);
             //modelBuilder.ApplyConfigurationsFromAssembly(typeof(EFContext).Assembly);
             //modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+            EFCoreHelper.OnDefaultModelCreating(modelBuilder);
         }
 
         public override int SaveChanges()
