600885991d15cc70369d29f45477e8b6015afeca
Mamunuz Zaman <mzs.munna@gmail.com>
2026-01-17 13:19:21 +0600

    code refactoring

--- mzstruct/Mzstruct.Auth/Managers/IdentityAuthManager.cs
+++ mzstruct/Mzstruct.Auth/Managers/IdentityAuthManager.cs
@@ -6,13 +6,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Mzstruct.Auth.Configs;
 using Mzstruct.Auth.Contracts.IManagers;
+using Mzstruct.DB.Contracts.IContext;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Mzstruct.Auth.Managers
 {
-    public class IdentityAuthManager<TIdentity>(UserManager<TIdentity> userManager,
+    public class IdentityAuthManager<TIdentity>(IAppDBContext appDBContext,
+        UserManager<TIdentity> userManager,
         SignInManager<TIdentity> signInManager,
         JwtTokenManager jwtTokenManager) : IIdentityAuthManager<TIdentity> where TIdentity : IdentityUser, new()
     {
@@ -24,15 +26,15 @@ namespace Mzstruct.Auth.Managers
                 Email = req.Email
             };
 
+            using var transaction = await appDBContext.Database.BeginTransactionAsync();       
             var result = await userManager.CreateAsync(user, req.Password);
             if (!result.Succeeded)
                 return string.Join(", ", result.Errors.Select(e => e.Description));
-
-            // Optional: assign default role
-            await userManager.AddToRoleAsync(user, "User");
-
+      
+            await userManager.AddToRoleAsync(user, "User"); // Optional: assign default role
             var roles = await userManager.GetRolesAsync(user);
             var token = jwtTokenManager.CreateIdentityToken(user, ["User"]);
+            await transaction.CommitAsync();
             return token;
         }
 
