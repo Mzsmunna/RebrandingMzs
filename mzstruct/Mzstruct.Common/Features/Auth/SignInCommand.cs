472b01f193b6657718d2e58ffbf8d27598b616be
Mamunuz Zaman <mzs.munna@gmail.com>
2026-01-31 10:50:39 +0600

    cqrs - first command - og

---
+++ mzstruct/Mzstruct.Common/Features/Auth/SignInCommand.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using Mzstruct.Auth.Contracts.IServices;
+using Mzstruct.Auth.Models.Dtos;
+using Mzstruct.Base.Contracts.ICommands;
+using Mzstruct.Base.Dtos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Mzstruct.Common.Features.Auth
+{
+    public record SignInCommand (
+        string Username,
+        [Required, EmailAddress] string Email, 
+        [Required, MinLength(3)] string Password
+    ) : ICommand<Result<string>>;
+
+    public class SignInCommandHandler(
+        //ILogger <SignInCommandHandler> logger,
+        IAuthService authService)
+        : ICommandHandler<SignInCommand, Result<string>>
+    {
+        public async Task<Result<string>> HandleAsync(SignInCommand command, CancellationToken token = default)
+        {
+            var dto = new SignInDto
+            (
+                command.Username,
+                command.Email,
+                command.Password
+            );
+            return await authService.SignIn(dto);
+        }
+    }
+}
