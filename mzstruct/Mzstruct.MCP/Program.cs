58340f8deca04981fab0ed5cc759a7a5d882b8f1
Mamunuz Zaman <mzs.munna@gmail.com>
2026-06-04 15:15:03 +0600

    my firist mcp?

---
+++ mzstruct/Mzstruct.MCP/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Mzstruct.Base.Entities;
+using Mzstruct.DB.Providers.MongoDB.Contracts.IRepos;
+using Mzstruct.DB.Providers.MongoDB.Repos;
+
+namespace Mzstruct.MCP
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, MCP!");
+            var builder = Host.CreateApplicationBuilder(args);
+
+            builder.Logging.AddConsole(options =>
+            {
+                options.LogToStandardErrorThreshold = LogLevel.Trace;
+            });
+
+            builder.Services.AddSingleton<IBaseUserRepository<BaseUser>, BaseUserRepository<BaseUser>>();
+            builder.Services.AddMcpServer().WithStdioServerTransport().WithToolsFromAssembly();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ mzstruct/Mzstruct.MCP/Tools/UserTools.cs
@@ -0,0 +1,32 @@
+﻿using ModelContextProtocol.Server;
+using Mzstruct.Base.Entities;
+using Mzstruct.DB.Providers.MongoDB.Contracts.IRepos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Mzstruct.MCP.Tools
+{
+    [McpServerToolType]
+    public class UserTools(IBaseUserRepository<BaseUser> repo)
+    {
+        [McpServerTool, Description("Get all users")]
+        public async Task<List<BaseUser>> GetAllUsers()
+        {    
+            return await repo.GetAll();
+        }
+
+        [McpServerTool, Description("Get user by ID")]
+        public async Task<BaseUser?> GetById([Description("The ID of the user to retrieve")] string id)
+        {
+            return await repo.GetById(id);
+        }
+
+        [McpServerTool, Description("Get user by field value")]
+        public async Task<List<BaseUser>> GetByFieldValue([Description("The name of the field to search by, can be any property of the user")] string fieldName, [Description("The value of the field to search for")] string fieldValue)
+        {
+            return await repo.GetByFieldValue(fieldName, fieldValue);
+        }
+    }
+}
